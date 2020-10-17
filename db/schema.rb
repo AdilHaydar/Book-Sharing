da60bbc7761a29d71a17e347ac1b3e3ad65bc346
Adil Haydar <adilhaydarr@gmail.com>
2020-10-17 16:21:01 +0300

    Database switched to PSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_10_14_172436) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.text "title"
     t.text "author"
